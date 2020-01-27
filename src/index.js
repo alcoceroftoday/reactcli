65bbece81b1f6dcf3830724f78936f2abb2f283c
David Alcocer <alcoceroftoday@gmail.com>
2020-01-27 14:49:06 -0500

    hola mundo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+// const element=React.createElement(componente,propiedades,hijos);
+// react sin rxfx
+// // const element=React.createElement("h1",{className:"saludo"},"H3ola mundo");
+const root=document.getElementById("root");
+// // ReactDOM.render(element,root);
+
+ReactDOM.render(<h1 className="saludos">Hola mundo</h1>,root)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
