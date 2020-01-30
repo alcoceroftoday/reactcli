c53ac7d48b541fb578b646da79d6a290a801a593
David Alcocer <davidbryan.alcocer@processconsulting.net>
2020-01-30 11:06:26 -0500

    se añadio el llamado import M from materialize-css;  para poder ejecutar materialize

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import "../styles/styles.scss";
 import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
+
+function createLink(user){
+return <p><a href="#">{user.link}</a></p>
+}
+const user = {
+    firstName: 'David',
+    lastName: 'Alcocer',    
+    title: 'Mi perfil',    
+    imgUrl: 'https://www.lavanguardia.com/r/GODO/LV/p5/WebSite/2018/02/01/Recortada/img_azarzoso_20180201-124709_imagenes_lv_propias_azarzoso_foto5-kjaF-U44445823725ekB-992x558@LaVanguardia-Web.jpg',    
+    link: 'www.dalcocer.com'
+};
+
 
 const Panel = () => (
 <>
+<div class="main">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src={user.imgUrl}/>
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">{user.title}<i class="material-icons right">Más +</i></span>
+      {createLink(user)}
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+  </div>
+</div>     
 
-    <a class="waves-effect waves-light btn-large">Button</a>
-        
-   {/* <div class="main-banner img-container " id="main-banner">
-       <div class="ed-grid lg-grid-8">
-           <div class="lg-cols-4 lg-x-2">
-               <img class="main-banner__img" src="https://www.lavanguardia.com/r/GODO/LV/p5/WebSite/2018/02/01/Recortada/img_azarzoso_20180201-124709_imagenes_lv_propias_azarzoso_foto5-kjaF-U44445823725ekB-992x558@LaVanguardia-Web.jpg"/>
-               <div class="main-banner__data s-center">
-                   <p class="t2 s-mb-0">a</p>
-                   <p>Subtitulo</p>
-                   <a href="#" class="button">contactame</a>
-               </div>
-
-           </div>
-       </div>
-   </div> */}
 </>
 )
 export default Panel;
